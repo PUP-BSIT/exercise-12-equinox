e9221779fcd89836fc721b26de08821380790295
Andrei James Alejandro <squishycaptain09@gmail.com>
2023-11-05 22:53:30 +0800

    Add comment function

--- alejandro_andreijames/script.js
+++ alejandro_andreijames/script.js
@@ -1,8 +1,9 @@
 let userName = document.getElementById("name");
 let userComment = document.getElementById("comment");
 let commentButton = document.getElementById("comment_button");
-   
-userName.addEventListener("input", buttonActivate)
+let commentsSection = document.getElementById("comments_section");
+
+userName.addEventListener("input", buttonActivate);
 userComment.addEventListener("input", buttonActivate);
 
 function buttonActivate() {
@@ -10,8 +11,25 @@ function buttonActivate() {
     let commentContentValue = userComment.value;
 
     if (nameContentValue !== "" && commentContentValue !== "") {
-         commentButton.removeAttribute("disabled");
+        commentButton.removeAttribute("disabled");
     } else {
-         commentButton.setAttribute("disabled", "disabled");
+        commentButton.setAttribute("disabled", "true");
     }
+}
+
+function addComment() {
+    let name = userName.value;
+    let comment = userComment.value;
+
+    if (name.trim() === '' || comment.trim() === '') {
+        return;
+    }
+
+    let commentElement = document.createElement('li');
+    commentElement.innerHTML = `<strong><em>${name}${comment}</em></strong>`;
+    commentsSection.appendChild(commentElement);
+
+    userName.value = '';
+    userComment.value = '';
+    commentButton.setAttribute('disabled', 'true');
 }
