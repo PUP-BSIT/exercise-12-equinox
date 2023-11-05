619f3c4e3b648be76718bef87babbd0a6e332802
Rylle <rylle1588@gmail.com>
2023-11-06 00:13:59 +0800

    Update File

--- jalipa_reannerylle/script.js
+++ jalipa_reannerylle/script.js
@@ -1,17 +1,35 @@
 let nameInput = document.getElementById("name");
-    let commentInput = document.getElementById("comment");
-    let commentButton = document.getElementById("comment-button");
+let commentInput = document.getElementById("comment");
+let commentButton = document.getElementById("comment_button"); 
    
-    nameInput.addEventListener("input", toggleCommentButton);
-    commentInput.addEventListener("input", toggleCommentButton);
+nameInput.addEventListener("input", toggleCommentButton);
+commentInput.addEventListener("input", toggleCommentButton);
 
-    function toggleCommentButton() {
-        let nameValue = nameInput.value;
-        let commentValue = commentInput.value;
+function toggleCommentButton() {
+    let nameValue = nameInput.value;
+    let commentValue = commentInput.value;
 
-        if (nameValue !== "" && commentValue !== "") {
-            commentButton.removeAttribute("disabled");
-        } else {
-            commentButton.setAttribute("disabled", "disabled");
-        }
+    if (nameValue.trim() !== "" && commentValue.trim() !== "") { 
+        commentButton.removeAttribute("disabled");
+    } else {
+        commentButton.setAttribute("disabled", "true"); 
     }
+}
+
+function addComment() {
+    let name = nameInput.value;
+    let comment = commentInput.value;
+
+    if (name.trim() === '' || comment.trim() === '') {
+        return;
+    }
+
+    let commentsSection = document.getElementById("comment_sec"); 
+    let commentElement = document.createElement('div');
+    commentElement.innerHTML = `<h4>${name}</h4><p>${comment}</p>`; 
+    commentsSection.appendChild(commentElement);
+
+    nameInput.value = '';
+    commentInput.value = '';
+    commentButton.setAttribute('disabled', 'true');
+}
