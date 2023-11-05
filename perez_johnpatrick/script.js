b55fb74a10e7bc5437aa6c6986db88d63541079a
JohnPatrickPerez <johnpatrickperezxx@gmail.com>
2023-11-05 22:03:05 +0800

    add file

--- perez_johnpatrick/script.js
+++ perez_johnpatrick/script.js
@@ -1,17 +1,36 @@
 let nameInput = document.getElementById("name");
-    let commentInput = document.getElementById("comment");
-    let commentButton = document.getElementById("comment-button");
-   
-    nameInput.addEventListener("input", toggleCommentButton);
-    commentInput.addEventListener("input", toggleCommentButton);
+let commentInput = document.getElementById("comment");
+let commentButton = document.getElementById("comment-button");
+let commentsSection = document.getElementById("comment_form");
 
-    function toggleCommentButton() {
-        let nameValue = nameInput.value;
-        let commentValue = commentInput.value;
+nameInput.addEventListener("input", toggleCommentButton);
+commentInput.addEventListener("input", toggleCommentButton);
+commentButton.addEventListener("click", addComment);
 
-        if (nameValue !== "" && commentValue !== "") {
-            commentButton.removeAttribute("disabled");
-        } else {
-            commentButton.setAttribute("disabled", "disabled");
-        }
+function toggleCommentButton() {
+    let nameValue = nameInput.value;
+    let commentValue = commentInput.value;
+
+    if (nameValue.trim() !== "" && commentValue.trim() !== "") {
+        commentButton.removeAttribute("disabled");
+    } else {
+        commentButton.setAttribute("disabled", "disabled");
     }
+}
+
+function addComment() {
+    let name = nameInput.value;
+    let comment = commentInput.value;
+
+    if (name.trim() === "" || comment.trim() === "") {
+        return;
+    }
+
+    let commentElement = document.createElement('div');
+    commentElement.innerHTML = `<p>${name}${comment}</p>`;
+    commentsSection.appendChild(commentElement);
+
+    nameInput.value = '';
+    commentInput.value = '';
+    commentButton.setAttribute('disabled', 'true');
+}
